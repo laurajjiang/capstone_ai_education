e76390e5fabab30bb3d50decda71458801f022d5
Laura Jiang <jianglau@oregonstate.edu>
2020-11-09 10:53:20 -0800

    add gitignore, navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  Anchor,
+  Box,
+  Grommet,
+  Header,
+  Nav,
+  Menu,
+  ResponsiveContext,
+} from "grommet";
+import theme from "./theme";
+
+const items = [
+  { label: "About", href: "/about" }
+];
+
+const CollapsableNav = () => (
+  <Grommet theme={theme}>
+    <Header
+      background="#FFFFFF"
+      pad="small"
+      border={{ side: "bottom", size: "small", color: "#000000" }}
+    >
+      <Box direction="row" align="center" gap="small">
+        <Anchor
+          margin={{ left: "medium" }}
+          color="dark-2"
+          size="xxlarge"
+          href={"/"}
+          label={"Interactive Visualization for AI Education"}
+          key={"home"}
+        />
+      </Box>
+      <ResponsiveContext.Consumer>
+        {(responsive) =>
+          responsive === "small" ? (
+            <Menu
+              label="Navigate"
+              items={[
+                {
+                  label: "About",
+                  onClick: (event) => {
+                    window.location.href = "/about";
+                  },
+                }
+              ]}
+            />
+          ) : (
+            <Nav direction="row">
+              {items.map((item) => (
+                <Anchor
+                  margin={{ left: "small", right: "medium" }}
+                  color="dark-2"
+                  size="large"
+                  href={item.href}
+                  label={item.label}
+                  key={item.label}
+                />
+              ))}
+            </Nav>
+          )
+        }
+      </ResponsiveContext.Consumer>
+    </Header>
+  </Grommet>
+);
+
+export default function Navigation() {
+  return <CollapsableNav />;
+}
