cf2b37c6b94e5f39ee31930bb3b74fc872d7dea7
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2020-11-11 13:50:09 -0800

    for demo

---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function SimpleFooter() {
+  return (
+        <div class="container">
+            <div class="row">
+                <div class="center">
+                    <div class="login_address">
+                    <h6>Advisor: Dr. Minsuk Kahng</h6>
+                    <h6>Written by Group 21 : Laura Jiang, Thuy-Vy Nguyen, Owen Taylor Markley, Junhyeok Jeong</h6>
+                    <h6>Oregon State University, Corvaills, OR, USA</h6>
+                        <a href="https://github.com/laurajjiang/capstone_ai_education" target="_blank">Github source code</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default SimpleFooter;
