53f104bdf40b7adfc8604e7bf712319639b85b45
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2020-11-11 15:02:42 -0800

    reset!

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import React,{Component} from "react";
+import React from "react";
 import Navigation from "./components/navbar";
 import SimpleFooter from "./components/footer";
-import Tf_VIS from "./components/tf";
 
 
-class App extends Component {
-  componentDidMount(){
-    const script1 = document.createElement("script");    
-    script1.async = true;
-    script1.src = "./components/data.js";    
-    this.div.appendChild(script1);
-
-    const script2 = document.createElement("script");    
-    script2.async = true;
-    script2.src = "./components/model.js";    
-    this.div.appendChild(script2);
-
-    const script3 = document.createElement("script");    
-    script3.async = true;
-    script3.src = "./components/tf-vis.js";    
-    this.div.appendChild(script3);
-
-  }
-
-  render(){
-    return(
-      <div>
-      <header><Navigation/></header> 
-      <Tf_VIS/>
-      <footer><SimpleFooter/></footer>
-      </div> 
-    )
-  }
+export default function App() {
+  return (
+    <div>
+      <Navigation />
+      {/* <Home /> */
+      <SimpleFooter /> }
+    </div>
+  );
 }
-
-export default App;
