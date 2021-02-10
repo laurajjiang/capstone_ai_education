fc5de4f9c4d2c5d1d160532a9be80a71910546c9
Laura Jiang <jianglau@oregonstate.edu>
2021-02-09 18:09:50 -0800

    add slider

--- src/components/confusionMatrix.js
+++ src/components/confusionMatrix.js
@@ -254,10 +254,50 @@ export default function ConfusionMatrix() {
         let rectId = "#rect" + id.substring(4);
         d3.selectAll(rectId).attr("fill", "pink");
       });
+
+    d3.select("#slider").on("change", function () {
+      console.log("SLIDER");
+      d3.select("svg").remove();
+      var currentValue = this.value;
+      d3.select("#textInput").text(currentValue);
+      for (var i = 0; i < data.length; i += 1) {
+        // i+=1 or i++ ?
+        var ugh = data[i]["confidence_score"][0];
+        console.log(ugh);
+        if (ugh < currentValue) {
+          table[parseInt(data[i]["true_label"])][
+            parseInt(data[i]["predicted_label"])
+          ] += 1;
+          dataset.push([
+            data[i]["true_label"],
+            data[i]["predicted_label"],
+            data[i]["text"],
+            data[i]["index"],
+          ]);
+        }
+      }
+    });
   }, []);
 
   return (
     <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}>
+        <input
+          id='slider'
+          type='range'
+          min='0'
+          max='1'
+          step='.1'
+          defaultValue='.5'
+          style={{ maxWidth: "50%" }}
+        />
+        <h2 id='textInput'>0.5</h2>
+      </div>
       <div style={{ display: "flex", flexDirection: "row" }}>
         <div id='matrix'> </div>
         <div id='review'>
