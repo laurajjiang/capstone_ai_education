fbe51eb2ad2017cd06ce371897dfc24d57b022a6
MarkleyO <markleyo@oregonstate.edu>
2021-05-18 11:47:35 -0700

    conflicting classes between barcode and vector

--- notebooks/libraries/vector.py
+++ notebooks/libraries/vector.py
@@ -2,29 +2,18 @@ from IPython.core.display import display, HTML
 from string import Template
 import json
 
-def say_hello():
-    display(HTML('<h1>Greetings</h1>'))
-
-
-# Desicion needs to be made, what is to be paresd to this library?
-# Should we send it a json? For now it will be a JSON
-#json filepath NEEDS to be repaired
-
 class Visualization:
-
     data_file_name = ""
 
     def __init__(self, json_name):
-           # JSON file must be located in libraries directory with python file. 
         self.data_file_name = "\"" + json_name + "\""
-
 
     d3_source_html = '<script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>'
 
     html_template = Template('''
         <style> $css_text </style>
         <h1> Vector Visualization </h1>
-        <div class="tables"></div>
+        <div class="data_table"></div>
         <script> $js_text </script>
     ''')
 
@@ -38,7 +27,6 @@ class Visualization:
       padding: 5px;
       width: 100%;
     }
-
     .headerRowStyle {
       background-color: #fff;
       border-bottom: 3px solid #ccc;
@@ -48,11 +36,9 @@ class Visualization:
       line-height: 14px;
       padding: 10px 5px 5px 5px
     }
-    
     .headerCellStyle {
       border-left: 1px solid #d0d4d5;
     }
-    
     .tableRowStyle {
       border-bottom: 1px solid #d0d4d5;
       color: #565656;
@@ -74,14 +60,12 @@ class Visualization:
             }
         }
 
-
         function checkNumberIfFloat(value) {
             return Number(value) === value && value % 1 !== 0;
         }
 
         d3.json($conf_data_filepath, function(dataSet) {
-            var div = d3.select('.tables');
-
+            var div = d3.select('.data_table');
             // append a table to the div
             var table = div.append("table")
                 .attr({
@@ -89,19 +73,15 @@ class Visualization:
                     class: 'table'
                 })
                 .classed("display", true);
-
             // append a header to the table
             var thead = table.append("thead")
-
             // append a body to the table
             var tbody = table.append("tbody")
-
             // append a row to the header
             var theadRow = thead.append("tr")
                 .attr({
                     class: 'headerRowStyle'
                 });
-
             // return a selection of cell elements in the header row
             // attribute (join) data to the selection
             // update (enter) the selection with nodes that have data
@@ -114,7 +94,6 @@ class Visualization:
                 .text(function(d) {
                     return d;
                 });
-
             // table body rows
             var tableBodyRows = tbody.selectAll("tr")
                 .data(dataSet)
@@ -123,7 +102,6 @@ class Visualization:
                 .attr({
                     class: 'tableRowStyle'
                 });
-
             //table body row cells
             tableBodyRows.selectAll("td")
                 .data(function(d) {
@@ -137,28 +115,21 @@ class Visualization:
                 .append(function(d) {
                     return createSVG(d);
                 });
-
         })
 
         function createSVG(d) {
             var w = 75;
             var h = 75;
-
             var kpi = document.createElement("div");
-
             var svg = d3.select(kpi).append("svg")
                 .attr({
                     width: w,
                     height: h
                 });
-        
             var elem = svg.selectAll("div")
                 .data([d]);
-
             var elemEnter = elem.enter()
                 .append("g");
-
-    
             if( checkNumberIfFloat(d) || Number.isInteger(d)){
                 var la = (d/7);
                 elemEnter.append("rect")
@@ -169,7 +140,6 @@ class Visualization:
                         height: 20
                     })
                     .style("fill", "#4078a9");
-
                 elemEnter.append("text")
                     .style("fill", "blue")
                     .attr("dy", 30)
@@ -182,7 +152,6 @@ class Visualization:
                         r: 20
                     })
                     .style("fill", colorPicker);
-
                 elemEnter.append("text")
                     .style("fill", "blue")
                     .attr("dy", 30)
@@ -192,15 +161,7 @@ class Visualization:
     }
     ''')
 
-    def display_internals(self):
-           print(self.css, "\n", self.html, "\n", self.js, self.data_file_name)
-
-    def html_test(self):
-           say_hello()
-
     def display(self):
-           display(HTML(self.d3_source_html))
-
-           js_text = self.js_template.substitute({'conf_data_filepath': self.data_file_name})
-
-           display(HTML(self.html_template.substitute({'css_text': self.css, 'js_text': js_text})))
+       display(HTML(self.d3_source_html))
+       js_text = self.js_template.substitute({'conf_data_filepath': self.data_file_name})
+       display(HTML(self.html_template.substitute({'css_text': self.css, 'js_text': js_text})))
